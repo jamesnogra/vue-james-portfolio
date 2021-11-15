307216ed18d10eb5b75fd6419d9e90311828bf33
n-146|USER <jamesnogra@gmail.com>
2021-11-15 17:44:44 +0800

    Added a link to share to FB and twitter.

--- src/components/TopicContainer.vue
+++ src/components/TopicContainer.vue
@@ -2,7 +2,15 @@
   <div class="topic-container" :style="{backgroundColor: this.bgColor}">
     <slot></slot>
     <div class="back-to-top">
-      <a href="#main-banner">↑ Back to Top ↑</a>
+      <a class="fixed-links" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fjames.iamcebu.com%2F&t=James Arnold E. Nogra Portfolio"
+         onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"
+         target="_blank" title="Share on Facebook">
+        Share to Facebook ⓕ
+      </a>
+      <a class="fixed-links" href="https://twitter.com/share?hashtags=awesome,sharing,portfolio&text=View the portfolio website of James Arnold Nogra&via=fallguyjames">
+        Share to Twitter ⓣ
+      </a>
+      <a class="fixed-links" href="#main-banner">Back to Top ↑</a>
     </div>
   </div>
 </template>
@@ -18,6 +26,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.fixed-links {
+  margin-right: 10px;
+}
 @media only screen and (min-width: 768px) {
   .topic-container {
     height: 100vh;
